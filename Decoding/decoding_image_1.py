af0f3118328c6c8231799969bb739e4cbfc91f2f
PaccyC <abayisengaaimepacifique@gmail.com>
2025-02-02 21:25:00 +0200

    added decoding codes

---
+++ Decoding/decoding_image_1.py
@@ -0,0 +1,6 @@
+from aspose.barcode import barcoderecognition
+
+reader = barcoderecognition.BarCodeReader("image-1.jpg", barcoderecognition.DecodeType.ALL_SUPPORTED_TYPES)
+recognized_results = reader.read_bar_codes()
+for barcode in recognized_results:
+    print(barcode.code_text)
